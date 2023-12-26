f414ff6b19521357940197e598c08157cc8781c0
IkBenJur <jurspammer@gmail.com>
2023-12-26 20:59:09 +0100

    Added padding margin and pading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="h-screen py-8 mx-3">
+    <main className="h-screen py-12 mx-6 flex flex-col gap-8">
       <div>
-        <h1 className="text-4xl">Hi, my name is Jur</h1>
-        <p>A student interested in fullstack development</p>
+        <h1 className="text-4xl">
+          Hi, my name is <span className="font-medium">Jur</span>
+        </h1>
+        <p className="text-xl my-2">A student interested in fullstack development</p>
       </div>
 
-      <div>
+      <div className="flex flex-col gap-8">
         <p>
           Welcome to my place on the internet currently I am finishing my
           studies as a Bio-Informatics student at the University of Applied
@@ -28,7 +30,10 @@ export default function Home() {
             projects
           </Link>{" "}
           or reach out to{" "}
-          <Link href={"mailto:jurbeijaard@gmail.com"} className="font-bold underline">
+          <Link
+            href={"mailto:jurbeijaard@gmail.com"}
+            className="font-bold underline"
+          >
             me
           </Link>
         </p>
